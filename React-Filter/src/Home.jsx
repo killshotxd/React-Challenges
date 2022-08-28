455f4fd4889c0c2d8cd659d0e8e3d587fb766d9a
killshotxd <hassanansari211@gmail.com>
2022-08-28 14:10:39 +0530

    Logic

--- React-Filter/src/Home.jsx
+++ React-Filter/src/Home.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { USERS } from "./User";
 const Home = () => {
-  return <div></div>;
+  const [name, setName] = useState("");
+
+  const [foundUsers, setFoundUsers] = useState(USERS);
+
+  const filter = (event) => {
+    const keyword = event.target.value;
+
+    if (keyword !== "") {
+      const results = USERS.filter((user) => {
+        return user.name.toLowerCase().startsWith(keyword.toLowerCase());
+      });
+      setFoundUsers(results);
+    } else {
+      setFoundUsers(USERS);
+    }
+
+    setName(keyword);
+  };
+
+  return (
+    <div className="container">
+      <input
+        type="search"
+        value={name}
+        onChange={filter}
+        className="input"
+        placeholder="Filter"
+      />
+
+      <div className="user-list">
+        {foundUsers && foundUsers.length > 0 ? (
+          foundUsers.map((user) => (
+            <li key={user.id} className="user">
+              <span className="user-id">{user.id}</span>
+              <span className="user-name">{user.name}</span>
+              <span className="user-age">{user.age} year old</span>
+            </li>
+          ))
+        ) : (
+          <h1>No results found!</h1>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
